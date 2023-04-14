24deb0341221e1e8712c16c9a8f3b189091ef1f3
Ema <emanuelmanrique@outlook.com>
2023-04-14 09:29:32 -0300

    Agrego Form cotizacion a pagina productos

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+    const form= document.getElementById('formCotizacion'); {
+
+  
+        form.addEventListener("submit", function(event) { 
+        event.preventDefault();
+
+        let trasactionFormData= new FormData(form);
+        let seleccionTabla= document.getElementById('tablaCotizar');
+        let nuevafila= seleccionTabla.insertRow(-1);
+        nuevafila.insertCell(0)
+        
+        nuevafila.insertCell(1)
+    
+    }
